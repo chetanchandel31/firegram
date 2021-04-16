92e079177d11bf27d84fb7b620a42f216505027f
chetanchandel31 <chetanchandel3@gmail.com>
2021-04-16 21:44:02 +0530

    extracted image into seperate component

---
+++ src/components/ImageGrid/Image/Image.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../../../firebase/firebase";
+
+const Image = ({ doc, setSelectedImage }) => {
+	const [user] = useAuthState(auth);
+	const collectionRef = firestore.collection("images");
+
+	const deleteHandler = id => {
+		collectionRef.doc(id).delete();
+	};
+
+	const likeHandler = (likesArr, id) => {
+		let likes = [...likesArr];
+		if (likesArr.includes(user.uid)) likes = likes.filter(el => el !== user.uid);
+		else likes.push(user.uid);
+		collectionRef.doc(id).set({ likes }, { merge: true });
+	};
+	return (
+		<div className="post">
+			<div className="postHeader">
+				<img src={doc.userPhotoUrl} className="userDp" alt="dp" />
+				<strong>{doc?.userName}</strong>
+			</div>
+			<div className="img-wrap" onClick={() => setSelectedImage(doc.url)}>
+				<img src={doc.url} alt="can't load" />
+			</div>
+			<div className="likeAndDeleteBar">
+				<button className="likeBtn" disabled={!user} onClick={() => likeHandler(doc.likes, doc.id)}>
+					like
+				</button>
+				{doc?.likes?.length}
+				<span></span>
+				{user?.uid === doc.creator && (
+					<button className="deleteBtn" onClick={() => deleteHandler(doc.id)}>
+						delete
+					</button>
+				)}
+			</div>
+			<div className="postDesc">
+				<strong>{doc.userName}</strong> {doc?.description}
+			</div>
+		</div>
+	);
+};
+
+export default Image;
--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,51 +1,14 @@
 import React from "react";
 import useFirestore from "../../hooks/useFirestore";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, firestore } from "../../firebase/firebase";
+import Image from "./Image/Image";
 
 const ImageGrid = ({ setSelectedImage }) => {
 	const { docs } = useFirestore("images");
-	const [user] = useAuthState(auth);
-	const collectionRef = firestore.collection("images");
-
-	const deleteHandler = id => {
-		collectionRef.doc(id).delete();
-	};
-
-	const likeHandler = (likesArr, id) => {
-		let likes = [...likesArr];
-		if (likesArr.includes(user.uid)) likes = likes.filter(el => el !== user.uid);
-		else likes.push(user.uid);
-		collectionRef.doc(id).set({ likes }, { merge: true });
-	};
 
 	return (
 		<div className="img-grid">
 			{docs?.map(doc => (
-				<div key={doc.id} className="post">
-					<div className="postHeader">
-						<img src={doc.userPhotoUrl} className="userDp" alt="dp" />
-						<strong>{doc?.userName}</strong>
-					</div>
-					<div className="img-wrap" onClick={() => setSelectedImage(doc.url)}>
-						<img src={doc.url} alt="can't load" />
-					</div>
-					<div className="likeAndDeleteBar">
-						<button className="likeBtn" disabled={!user} onClick={() => likeHandler(doc.likes, doc.id)}>
-							like
-						</button>
-						{doc?.likes?.length}
-						<span></span>
-						{user?.uid === doc.creator && (
-							<button className="deleteBtn" onClick={() => deleteHandler(doc.id)}>
-								delete
-							</button>
-						)}
-					</div>
-					<div className="postDesc">
-						<strong>{doc.userName}</strong> {doc?.description}
-					</div>
-				</div>
+				<Image key={doc.id} doc={doc} setSelectedImage={setSelectedImage} />
 			))}
 		</div>
 	);
