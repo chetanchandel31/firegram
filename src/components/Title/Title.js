a1205f9d17d8f7d525aba004d0cd399226af0374
chetanchandel31 <chetanchandel3@gmail.com>
2021-04-17 18:58:23 +0530

    add dummy user

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,18 +4,30 @@ import firebase, { auth } from "../../firebase/firebase";
 
 const Title = () => {
 	const [user] = useAuthState(auth);
-	// console.log(user);
+	console.log(auth.currentUser);
 
 	const authHandler = () => {
 		if (user) auth.signOut();
 		else auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
 	};
 
+	const dummySigninHandler = () => {
+		auth.signInWithEmailAndPassword("abc@def.com", "12341234");
+	};
+
+	// const updateUser = () => {
+	// 	auth.currentUser.updateProfile({
+	// 		displayName: "dummy user",
+	// 		photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsmsT0Mla29mAwNqzAWyLjRBX3xsZFKue7ig&usqp=CAU",
+	// 	});
+	// };
+	// updateUser();
+
 	return (
 		<div className="title">
 			<div className="header">
 				{user && <img src={user.photoURL} alt="dp" />}
-				<button onClick={authHandler}>{user ? "logout" : "google sign in"}</button>
+				<button onClick={authHandler}>{user ? "logout" : "google sign in"}</button> {!user && <button onClick={dummySigninHandler}>dummy sign in</button>}
 			</div>
 			<h1>FireGram</h1>
 			{!user && <p>You can sign in with google to create posts and like other's posts</p>}
